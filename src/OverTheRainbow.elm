87c4c1aeb6d849a89eae3d6c11145bb6af5d2206
SetraR <setrar@valiha.com>
2021-05-24 11:54:38 -0400

    .

---
+++ src/OverTheRainbow.elm
@@ -0,0 +1,82 @@
+module OverTheRainbow exposing (main)
+
+import Browser
+import Chords exposing (Voicing)
+import Chords.Chart
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Instruments.Guitar as Guitar
+
+
+type alias Model =
+    { chords : List String }
+
+
+initialModel : Model
+initialModel =
+-- { chords = [ "C" , "G" , "Am" , "F" ] } -- intro1
+-- { chords = [ "C", "Em", "F", "C", "F", "E7", "Am", "F" ] } -- intro 2
+  { chords = [ "C" , "Em" , "F" , "C", "F", "C", "G", "Am", "F" ] } -- verse
+--  { chords = [ "C" , "Em" , "Am" , "F" ] } -- bridge
+
+
+type Msg
+    = NoOp
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
+
+
+view : Model -> Html msg
+view model =
+    let
+        guitar =
+            { tuning = Guitar.defaultTuning
+            , numFrets = 10
+            }
+
+        content =
+            model.chords
+                |> List.map (\elem -> ( elem, Chords.parseChord elem ))
+                |> List.map
+                    (\( name, result ) ->
+                        case result of
+                            Ok chord ->
+                                Guitar.voicings guitar chord
+                                    |> List.head
+                                    |> Maybe.map (viewChart name)
+                                    |> Maybe.withDefault
+                                        (Html.text ("Could not find voicing for " ++ name))
+
+                            Err err ->
+                                Html.text ("Could not parse chord " ++ name)
+                    )
+    in
+    Html.div
+        [ Attr.style "display" "flex"
+        , Attr.style "flexDirection" "row"
+        , Attr.style "flexWrap" "wrap"
+        ]
+        content
+
+
+viewChart : String -> Voicing -> Html msg
+viewChart name voicing =
+    Html.div
+        [ Attr.style "width" "150px"
+        ]
+        [ Chords.Chart.view name voicing
+        ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
